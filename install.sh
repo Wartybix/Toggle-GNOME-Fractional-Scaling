12bc37050ea182db159e190577289bd41e4d8b14
wartybix <34974060+Wartybix@users.noreply.github.com>
2024-03-04 17:45:20 +0000

    Scaling can now be toggled from the GNOME launcher

---
+++ install.sh
@@ -0,0 +1,5 @@
+mkdir ~/.local/toggle-fractional-scaling
+cp enable_frac.sh disable_frac.sh -p ~/.local/toggle-fractional-scaling/
+cp disable-frac-scaling.desktop enable-frac-scaling.desktop ~/.local/share/applications/
+
+echo "Installed 😃"
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+rm -dr ~/.local/toggle-fractional-scaling
+rm ~/.local/share/applications/disable-frac-scaling.desktop ~/.local/share/applications/enable-frac-scaling.desktop
+
+echo "Uninstalled 😿"
